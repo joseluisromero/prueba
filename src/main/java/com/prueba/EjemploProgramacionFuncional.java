02287179738e868718440d7c020eb955ab0af7c4
Jose Romero <manabis.rum@hotmail.com>
2022-05-30 19:49:04 -0500

    expresiones lambda

---
+++ src/main/java/com/prueba/EjemploProgramacionFuncional.java
@@ -0,0 +1,40 @@
+package com.prueba;
+
+import com.prueba.entity.Car;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class EjemploProgramacionFuncional {
+    public static void main(String[] args) {
+        Car car = new Car(UUID.randomUUID(), true, "Json", 2);
+        Car car2 = new Car(UUID.randomUUID(), false, "xml", 5);
+        Car car3 = new Car(UUID.randomUUID(), false, "xml", null);
+        List<Car> listCar = new ArrayList<>();
+        listCar.add(car);
+        listCar.add(car2);
+        listCar.add(car3);
+        //para filtrar los objetos  no null y imprimir por consola el campo pasajero
+        listCar.stream().filter((car1) -> Objects.nonNull(car1)).map(Car::getNumPasajero).forEach(System.out::println);
+        //para agregarlo a otra lista los objetos nonNull
+        List<Car> carList = listCar.stream().filter(car1 -> Objects.nonNull(car1)).collect(Collectors.toList());
+        //Para agregar a otra lista los objetos que tienen el pajaro no null
+        List<Car> carListPasajero = listCar.stream().filter(car1 -> Objects.nonNull(car1.getNumPasajero())).collect(Collectors.toList());
+        //generar una lista de 100 numeros
+        IntStream intStream = IntStream.range(0, 100);
+        //intStream.forEach(System.out::println);
+        //sumar los numero impares
+        intStream.filter(value -> value % 2 == 0).map(operand -> operand * operand).reduce(Integer::sum).ifPresent(System.out::println);
+        //OptionalInt suma=intStream.filter(value -> value%2==0).map(operand -> operand*operand).reduce(Integer::sum).ifPresent(System.out::println);
+        int total = 0;
+        float totalSimple = 0;
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0) {
+                totalSimple = (float) (totalSimple +Math.pow(i, 2));
+            }
+            total += Math.pow(i, 2);
+        }
+        System.out.println(total + " total simple " + totalSimple);
+    }
+}
---
+++ src/main/java/com/prueba/EjemploStreamForEachMap.java
@@ -0,0 +1,53 @@
+package com.prueba;
+
+import com.prueba.entity.Car;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class EjemploStreamForEachMap {
+    public static void main(String[] args) {
+        Car car = new Car(UUID.randomUUID(), true, "Json", 2);
+        Car car2 = new Car(UUID.randomUUID(), false, "xml", 5);
+        Car car3 = new Car(UUID.randomUUID(), false, "text", 7);
+        List<Car> listCar = new ArrayList<>();
+        listCar.add(car);
+        listCar.add(car2);
+        listCar.add(car3);
+        //Diferencia estre forEach vs StreamForEach
+        //1.) forEach
+        listCar.forEach((c) -> {
+            System.out.println(c.getModelo());
+            System.out.println(String.valueOf(c.getNumPasajero()));
+            System.out.println(String.valueOf(c.isEncendido()));
+        });
+        //2.) es Cierto que Stream podemos hacer uso de map o filter que le dan una mayor flexibilidad
+        listCar.stream().forEach((c) -> {
+            System.out.println(c.getModelo());
+            System.out.println(String.valueOf(c.getNumPasajero()));
+            System.out.println(String.valueOf(c.isEncendido()));
+        });
+
+        //3. pero lo que si es  notoria seria si tenemos un  diccionario con Personas (hashmap)
+        Map<String, Car> map = new HashMap<>();
+        map.put(car.getModelo(), car);
+        map.put(car2.getModelo(), car);
+        map.put(car3.getModelo(), car);
+
+        //3.1 recorreclo con forEach
+        for (String model : map.keySet()) {
+            Car carSeleccionado = map.get(model);
+            System.out.println(carSeleccionado.getModelo());
+            System.out.println(String.valueOf(carSeleccionado.getNumPasajero()));
+            System.out.println(String.valueOf(carSeleccionado.isEncendido()));
+        }
+
+        //3.2 pero si lo hacemos  forEach
+        map.forEach((k, v) -> {
+            System.out.println(v.getModelo());
+            System.out.println(v.getNumPasajero());
+            System.out.println(v.isEncendido());
+        });
+    }
+}
