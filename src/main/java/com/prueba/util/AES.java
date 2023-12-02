3cb98980f6cb57bb541bcc65ccba9be743b2e2b2
joromero <joromero@pichincha.com>
2023-12-02 00:36:28 -0500

    codigo de test

--- src/main/java/com/prueba/util/AES.java
+++ src/main/java/com/prueba/util/AES.java
@@ -30,8 +30,11 @@ public class AES {
 
 
     public static String encryptAES(String text) throws Exception {
-        String key = "92AE31A79FEEB2A3"; //llave
-        String iv = "0123456789ABCDEF"; // vector de inicialización
+        //String key = "92AE31A79FEEB2A3"; //llave
+        //String iv = "0123456789ABCDEF"; // vector de inicialización*/
+        //PARA APIGEE
+        String key = "oPDomhSy6nycDWS1AO5zhrcRSlf2WSEV"; //llave
+        String iv = "cGmtg3KWsIgeldgz"; // vector de inicialización*/
         return encrypt(key, iv, text);
     }
 
